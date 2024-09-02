1d58d2d6c24e3e0edc8bf60ca66945b4a9fd7b8f
Matheus <matheussantossilvadd@gmail.com>
2024-09-02 20:46:56 -0300

    VerificacaoMaiorIdade

---
+++ TrabalhoCleito/src/Temperatura.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Temperatura
+{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Digite a temperatura em graus Celsius: ");
+        double temperatura = scanner.nextDouble();
+
+
+        if (temperatura > 30) {
+
+            System.out.println("Classificação: Quente");
+        } else if (temperatura >= 15) {
+
+            System.out.println("Classificação: Agradável");
+        } else {
+
+            System.out.println("Classificação: Frio");
+        }
+
+    }
+}
+
+
+
+}
