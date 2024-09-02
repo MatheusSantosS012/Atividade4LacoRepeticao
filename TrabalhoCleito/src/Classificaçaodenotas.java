b95374034ea49384245ae216494170da0edc01e5
Matheus <matheussantossilvadd@gmail.com>
2024-09-02 20:23:06 -0300

    Classificaçãodenotas

---
+++ TrabalhoCleito/src/Classificaçaodenotas.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Classificaçaodenotas {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite sua nota de 0 a 10");
+
+        double nota = scanner.nextDouble();
+
+
+        if (nota >= 9) {
+
+            System.out.println("Classificação: Excelente");
+        } else if (nota >= 7) {
+
+            System.out.println("Classificação: Bom");
+        } else if (nota >= 5) {
+
+            System.out.println("Classificação: Satisfatório");
+        } else {
+
+            System.out.println("Classificação: Insatisfatório");
+        }
+    }
+}
+
